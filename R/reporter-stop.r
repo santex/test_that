734ad977238739e702c70a25a8d6a8847767522e
Ken Williams <Ken.Williams@WindLogics.com>
2012-03-26 13:10:47 -0500

    Adjust wording of test failure message, as per https://github.com/hadley/test_that/issues/36

--- R/reporter-stop.r
+++ R/reporter-stop.r
@@ -38,7 +38,7 @@ StopReporter <- setRefClass("StopReporter", contains = "Reporter",
       }
       failures <<- list()
 
-      msg <- str_c("Test failure in '", cur_test, "'\n", messages)
+      msg <- str_c("Testing whether '", cur_test, "'\n", messages)
       stop(msg, call. = FALSE)
     },
 
